de8b81f206bb4e3d383822516339715b09013e7f
Jacob_Meline <jacob.meline@gmail.com>
2014-03-19 21:27:45 -0600

    updated singleton with pytests

--- Singleton/singleton.py
+++ Singleton/singleton.py
@@ -1,13 +1,14 @@
 # # # # # # # # # # # # # # # # # # # # # # # # #
-#		Design Pattern: Singleton       #
+#		Design Pattern: Singleton       		#
 # singleton is a design pattern where only one  #
-#	instance may exist at any given time    #
+#	instance may exist at any given time    	#
 # # # # # # # # # # # # # # # # # # # # # # # # #	
 
 # Refer here to for more information
 #	https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 
 # first way to do a singleton through hard coding
+
 class MySingleton(object):
 	_instance = None
 	def __new__(self):
@@ -18,17 +19,6 @@ class MySingleton(object):
 			self.y = 10
 
 		return self._instance
-print "Singleton Example1"
-x = MySingleton()
-print x.y
-assert x.y == 10
-
-x.y = 20
-assert x.y != 10
-z = MySingleton()
-print z.y
-assert x.y == z.y
-
 
 # Second way to do a singleton using a decorator
 def singleton(MyClass):
@@ -44,19 +34,7 @@ class TestClass(object):
 	def __init__(self):
 		self.value = 10
 
-print "Singleton Example2"
-x = TestClass()
-assert x.value == 10
-print x.value
-x.value *= 10
-
-assert x.value != 10
-assert x.value == 100
-y = TestClass()
-print y.value
-assert x.value == y.value
-
-# Third way to do a singleton using metaclasses
+# Third way to do a singleton using metaclasses (I like this way)
 class Singleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
@@ -64,11 +42,43 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
-#Python2
-class MyClass(BaseClass):
+# Python2 example
+class MyClass(object):
     __metaclass__ = Singleton
 
 #Python3
-class MyClass(BaseClass, metaclass=Singleton):
-    pass
+#class MyClass(BaseClass, metaclass=Singleton):
+#    pass
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#	Final Challenges by Youtube Channel Trevor Payne 	#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+## 	Create a singleton that has a function which returns a value that increase +1 everytime it is called (E.X. Returns 1,2,3,4,5)
+class ChallengeOne():
+	__metaclass__ = Singleton
+
+	def __init__(self):
+		print (self.__class__.__name__ + " Instantiated")
+		self.count = 0
+
+	def update(self):
+		self.count += 1
+		return self.count
+
+## Create a singleton that acts like a phonebook. It has a function that you can pass in a Name and a Number to store, 
+## 	and another function to retrieve a Number and a Given Name
+class ChallengeTwo():
+	__metaclass__ = Singleton
+
+	def __init__(self):
+		self.name = ''
+		self.number = 0
+		print (self.__class__.__name__ + " Instantiated")
+
+	def updatePhoneBook(self, name, number):
+		self.name = name
+		self.number = number
+
+	def getPhoneBook(self):
+		return self.name, self.number
